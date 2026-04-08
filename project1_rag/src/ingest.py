0d3ef696122aab686a5b505cec8b545215b3bf14
Krishna Damaraju <krishnadamaraju@Krishnas-MacBook-Pro.local>
2026-04-08 16:51:49 -0400

    Fix ingest imports and add resume to data

--- project1_rag/src/ingest.py
+++ project1_rag/src/ingest.py
@@ -21,7 +21,7 @@ from langchain_community.document_loaders import (
     UnstructuredMarkdownLoader,
     PyMuPDFLoader,
 )
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from dotenv import load_dotenv
